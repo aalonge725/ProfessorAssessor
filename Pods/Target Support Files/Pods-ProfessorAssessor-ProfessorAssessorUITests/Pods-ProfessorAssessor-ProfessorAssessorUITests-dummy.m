7ae4089e3374d39f4c376b3557b510642b35ff3d
Abraham Alonge <aalonge@unc.edu>
2021-07-12 17:52:02 -0400

    Installed Parse pod, implemented login and logout, and began implementing signup

---
+++ Pods/Target Support Files/Pods-ProfessorAssessor-ProfessorAssessorUITests/Pods-ProfessorAssessor-ProfessorAssessorUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ProfessorAssessor_ProfessorAssessorUITests : NSObject
+@end
+@implementation PodsDummy_Pods_ProfessorAssessor_ProfessorAssessorUITests
+@end
---
+++ ProfessorAssessor/View Controllers/LoginViewController.m
@@ -0,0 +1,56 @@
+#import "LoginViewController.h"
+#import "Parse/Parse.h"
+
+@interface LoginViewController ()
+
+@property (strong, nonatomic) IBOutlet UITextField *username;
+@property (strong, nonatomic) IBOutlet UITextField *password;
+
+- (IBAction)didTapLogin:(UIButton *)sender;
+- (IBAction)didTapSignUp:(UIButton *)sender;
+- (IBAction)didTapSignUpWithFacebook:(UIButton *)sender;
+
+@end
+
+@implementation LoginViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+- (IBAction)didTapLogin:(UIButton *)sender {
+    NSString *username = self.username.text;
+    NSString *password = self.password.text;
+
+    // TODO: check if username and password are valid before method call
+    [PFUser logInWithUsernameInBackground:username password:password block:^(PFUser * user, NSError *  error) {
+        if (error != nil) {
+            // TODO: handle error
+        } else {
+            // TODO: manually segue to logged in view
+            [self performSegueWithIdentifier:@"loginSegue" sender:self];
+        }
+    }];
+}
+
+- (IBAction)didTapSignUp:(UIButton *)sender {
+    PFUser *newUser = [PFUser user];
+
+    newUser.username = self.username.text;
+    newUser.password = self.username.text;
+
+    // TODO: check if username and password are valid before method call
+    [newUser signUpInBackgroundWithBlock:^(BOOL succeeded, NSError * error) {
+        if (error != nil) {
+            // TODO: handle error
+        } else {
+            // TODO: manually segue to register view
+        }
+    }];
+}
+
+- (IBAction)didTapSignUpWithFacebook:(UIButton *)sender {
+
+}
+
+@end
