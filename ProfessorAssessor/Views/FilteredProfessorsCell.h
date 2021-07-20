c6c4aaa2168b108309baa2f0db883a09998f590f
Abraham Alonge <aalonge@unc.edu>
2021-07-20 19:49:40 -0400

    Added outlets for table view cells on home page

--- ProfessorAssessor/Views/FilteredProfessorsCell.h
+++ ProfessorAssessor/Views/FilteredProfessorsCell.h
@@ -5,6 +5,9 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface FilteredProfessorsCell : UITableViewCell
 
+@property (strong, nonatomic) IBOutlet UILabel *professorName;
+@property (strong, nonatomic) IBOutlet UILabel *departmentName;
+@property (strong, nonatomic) IBOutlet UIView *averageRating;
 
 - (void)setProfessor:(Professor *)professor;
 
--- ProfessorAssessor/Views/SortedProfessorsCell.h
+++ ProfessorAssessor/Views/SortedProfessorsCell.h
@@ -5,6 +5,9 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface SortedProfessorsCell : UITableViewCell
 
+@property (strong, nonatomic) IBOutlet UILabel *professorName;
+@property (strong, nonatomic) IBOutlet UILabel *departmentName;
+@property (strong, nonatomic) IBOutlet UIView *averageRating;
 
 - (void)setProfessor:(Professor *)professor;
 
