fc6996ea648387b2209af3cb202857987a8143a7
Abraham Alonge <aalonge@unc.edu>
2021-07-21 18:02:45 -0400

    Implemented home page table views and ability to search for a professor

--- ProfessorAssessor/View Controllers/HomeViewController.m
+++ ProfessorAssessor/View Controllers/HomeViewController.m
@@ -5,11 +5,16 @@
 #import "Parse/Parse.h"
 #import "SceneDelegate.h"
 #import "Networker.h"
+#import "FilteredProfessorsCell.h"
+#import "SortedProfessorsCell.h"
 #import "School.h"
 #import "Professor.h"
 
 @interface HomeViewController () <UITableViewDataSource, UITableViewDelegate, UISearchBarDelegate>
 
+@property (strong, nonatomic) IBOutlet UITableView *filteredTableView;
+@property (strong, nonatomic) IBOutlet UITableView *sortedTableView;
+@property (strong, nonatomic) IBOutlet UISearchBar *searchBar;
 @property (strong, nonatomic) School *school;
 @property (strong, nonatomic) NSArray<Professor *> *professors;
 @property (strong, nonatomic) NSArray<Professor *> *filteredProfessors;
@@ -24,21 +29,90 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
 
-    [self fetchSchoolAndProfessors];
+    [self fetchProfessors];
 }
 
-- (void)fetchSchoolAndProfessors {
+- (void)viewWillAppear:(BOOL)animated {
+    NSIndexPath *selectedFromFiltered = self.filteredTableView.indexPathForSelectedRow;
+    NSIndexPath *selectedFromSorted = self.sortedTableView.indexPathForSelectedRow;
+
+    if (selectedFromFiltered) {
+        [self.filteredTableView deselectRowAtIndexPath:selectedFromFiltered animated:YES];
+    }
+
+    if (selectedFromSorted) {
+        [self.sortedTableView deselectRowAtIndexPath:selectedFromSorted animated:YES];
+    }
+}
+
+- (void)fetchProfessors {
     __weak typeof(self) weakSelf = self;
 
-    [Networker fetchSchoolAndProfessorsWithCompletion:^(PFObject *_Nullable object, NSError *_Nonnull error) {
+    [Networker
+     fetchSchoolAndProfessorsWithCompletion:^(
+                                              PFObject *_Nullable object,
+                                              NSError *_Nonnull error) {
         if (object) {
             weakSelf.school = [School schoolFromPFObject:object];
-            weakSelf.professors = weakSelf.school.professors;
+            
+            NSArray<Professor *> *professors = weakSelf.school.professors;
+            weakSelf.professors = [professors
+                                   sortedArrayUsingComparator:
+                                   ^NSComparisonResult(School *_Nonnull school1,
+                                                       School *_Nonnull school2) {
+                return [school1.name compare:school2.name];
+            }];
             weakSelf.filteredProfessors = weakSelf.professors;
+            weakSelf.sortedProfessors = weakSelf.professors;
 
-            // TODO: reload tableView
+            [self.filteredTableView reloadData];
+            [self.sortedTableView reloadData];
         }
     }];
+}
+
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return tableView == self.filteredTableView ? self.filteredProfessors.count : self.sortedProfessors.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    if (tableView == self.filteredTableView) {
+        FilteredProfessorsCell *cell = [tableView
+                                        dequeueReusableCellWithIdentifier:@"FilteredProfessorsCell"
+                                        forIndexPath:indexPath];
+
+        Professor *professor = self.filteredProfessors[indexPath.row];
+        [cell setProfessor:professor];
+
+        return cell;
+    } else if (tableView == self.sortedTableView) {
+        SortedProfessorsCell *cell = [
+                                      tableView
+                                      dequeueReusableCellWithIdentifier:@"SortedProfessorsCell"
+                                      forIndexPath:indexPath];
+
+        Professor *professor = self.sortedProfessors[indexPath.row];
+        [cell setProfessor:professor];
+
+        return cell;
+    }
+    return [UITableViewCell new];
+}
+
+- (void)searchBar:(UISearchBar *)searchBar textDidChange:(NSString *)searchText {
+    if (searchText.length != 0) {
+        NSPredicate *predicate = [NSPredicate
+                                  predicateWithBlock:
+                                  ^BOOL(id _Nullable evaluatedObject,
+                                        NSDictionary<NSString *,id> *_Nullable bindings) {
+            return [((Professor *)evaluatedObject).name containsString:searchText];
+        }];
+        self.filteredProfessors = [self.professors filteredArrayUsingPredicate:predicate];
+    } else {
+        self.filteredProfessors = self.professors;
+    }
+    [self.filteredTableView reloadData];
 }
 
 - (IBAction)logout:(UIBarButtonItem *)sender {
@@ -51,7 +125,9 @@
 
 - (void)displayLoginPage {
     UIStoryboard *storyboard = [UIStoryboard storyboardWithName:@"Main" bundle:nil];
-    LogInOrSignUpViewController *viewController = [storyboard instantiateViewControllerWithIdentifier:@"LogInOrSignUpViewController"];
+    LogInOrSignUpViewController *viewController = [storyboard
+                                                   instantiateViewControllerWithIdentifier:
+                                                   @"LogInOrSignUpViewController"];
 
     SceneDelegate *sceneDelegate = (SceneDelegate *)self.view.window.windowScene.delegate;
 
